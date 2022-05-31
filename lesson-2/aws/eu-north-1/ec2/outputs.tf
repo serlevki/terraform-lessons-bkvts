7084f42a45ed0a15e65f7ee4f7b3d7bbcda63dc6
serhio <lewkivskiy95@gmail.com>
2022-05-31 23:49:21 +0300

    leave only third lesson

--- lesson-2/aws/eu-north-1/ec2/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "id" {
-  description = "The ID of the instance"
-  value       = module.ec2_instance.id
-}
--- lesson-2/aws/eu-north-1/vpc/variables.tf
+++
@@ -1,4 +0,0 @@
-variable "aws_region" {
-  description = "AWS Region"
-  default     = "eu-north-1"
-}
