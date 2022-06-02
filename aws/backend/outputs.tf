1f0af98af860fd1220fb459c9debfa39fb0c0a0c
serhio <lewkivskiy95@gmail.com>
2022-06-03 00:40:33 +0300

    trigger

--- aws/backend/outputs.tf
+++ aws/backend/outputs.tf
@@ -5,5 +5,5 @@ output "s3_bucket_id" {
 
 output "dynamodb_table_name" {
   value       = aws_dynamodb_table.terraform_state_locks.name
-  description = "The name of the DynamoDB table"
+  description = "The name of the ynamoDB table"
 }
